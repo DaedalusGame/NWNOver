f4162a6c73265c00d32c11220a7af98d49b720aa
BordListian <bordlistian@hotmail.de>
2018-12-30 13:37:59 +0100

    - Adds more Schema support

--- NWNOver/TwoDASchemaDatabase.cs
+++ NWNOver/TwoDASchemaDatabase.cs
@@ -121,20 +121,83 @@ namespace NWNOver
 
             currentSchema = new TwoDASchema();
             currentSchema.LabelColumn = 1;
-            AddSchema("spells", currentSchema);
+            AddSchema("categories", currentSchema);
 
             currentSchema = new TwoDASchema();
             currentSchema.LabelColumn = 1;
+            currentSchema.AddColumn(new StrRefColumn(2));
+            currentSchema.AddColumn(new StrRefColumn(3));
+            currentSchema.AddColumn(new FileRefColumn(4, "{0}.tga"));
+            currentSchema.AddColumn(new TwoDARefColumn(13, "feat.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(14, "feat.2da"));
+            //currentSchema.AddColumn(new TwoDARefColumn(14, "feat.2da"));
+            currentSchema.AddColumn(new BoolColumn(15));
+            currentSchema.AddColumn(new BoolColumn(16));
+            currentSchema.AddColumn(new BoolColumn(17));
+            currentSchema.AddColumn(new TwoDARefColumn(18, "categories.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(20, "spells.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(21, "feat.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(24, "masterfeats.2da"));
+            currentSchema.AddColumn(new BoolColumn(25));
+            currentSchema.AddColumn(new TwoDARefColumn(26, "feat.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(27, "feat.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(28, "feat.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(29, "feat.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(30, "feat.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(31, "skills.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(33, "skills.2da"));
+            currentSchema.AddColumn(new EnumColumn(36, new string[] { "1", "2", "3", "4", "5", "6", null }, new Dictionary<string, string>()
+            {
+                { "1" , "1 - combat feat" },
+                { "2" , "2 - active combat feat" },
+                { "3" , "3 - defensive feat" },
+                { "4" , "4 - magical feat" },
+                { "5" , "5 - class/racial feat" },
+                { "6" , "6 - other feat" },
+            }));
+            currentSchema.AddColumn(new BoolColumn(37));
+            currentSchema.AddColumn(new BoolColumn(42));
+            currentSchema.AddColumn(new BoolColumn(43));
             AddSchema("feat", currentSchema);
 
             currentSchema = new TwoDASchema();
             currentSchema.LabelColumn = 1;
             currentSchema.AddColumn(new StrRefColumn(2));
+            currentSchema.AddColumn(new StrRefColumn(3));
+            currentSchema.AddColumn(new FileRefColumn(4, "{0}.tga"));
+            currentSchema.AddColumn(new BoolColumn(5));
+            currentSchema.AddColumn(new EnumColumn(6, new string[] { "STR", "DEX", "CON", "INT", "WIS", "CHA", null }));
+            currentSchema.AddColumn(new BoolColumn(7));
+            currentSchema.AddColumn(new BoolColumn(8));
+            currentSchema.AddColumn(new TwoDARefColumn(9, "categories.2da"));
+            currentSchema.AddColumn(new BoolColumn(12));
+            AddSchema("skills", currentSchema);
+
+            currentSchema = new TwoDASchema();
+            currentSchema.LabelColumn = 1;
+            currentSchema.AddColumn(new StrRefColumn(2));
+            currentSchema.AddColumn(new EnumColumn(7, new string[] { "STR", "DEX", "CON", "INT", "WIS", "CHA", null }));
+            currentSchema.AddColumn(new EnumColumn(11, new string[] { "STR", "DEX", "CON", "INT", "WIS", "CHA", null }));
+            currentSchema.AddColumn(new BoolColumn(14));
             AddSchema("poison", currentSchema);
 
             currentSchema = new TwoDASchema();
             currentSchema.LabelColumn = 1;
             currentSchema.AddColumn(new StrRefColumn(2));
+            var enumDiseaseType = new string[] { "0", "1", "2", "3", "4", "5", null };
+            var descDiseaseType = new Dictionary<string, string>()
+            {
+                { "0" , "0 - STR" },
+                { "1" , "1 - DEX" },
+                { "2" , "2 - CON" },
+                { "3" , "3 - INT" },
+                { "4" , "4 - WIS" },
+                { "5" , "5 - CHA" },
+            };
+            currentSchema.AddColumn(new EnumColumn(8, enumDiseaseType, descDiseaseType));
+            currentSchema.AddColumn(new EnumColumn(11, enumDiseaseType, descDiseaseType));
+            currentSchema.AddColumn(new EnumColumn(14, enumDiseaseType, descDiseaseType));
+            currentSchema.AddColumn(new EnumColumn(15, new string[] { "EXTRA", "SUPER", null }));
             AddSchema("disease", currentSchema);
 
             currentSchema = new TwoDASchema();
@@ -454,6 +517,122 @@ namespace NWNOver
             currentSchema.LabelColumn = 1;
             currentSchema.AddColumn(new StrRefColumn(3));
             AddSchema("creaturesize", currentSchema);
+
+            currentSchema = new TwoDASchema();
+            currentSchema.LabelColumn = 1;
+            currentSchema.AddColumn(new EnumColumn(2, new string[] { "F", "D", "P", "B", null }, new Dictionary<string, string>()
+            {
+                { "F", "F - Fire and Forget" },
+                { "D", "D - Duration" },
+                { "P", "P - Projectile" },
+                { "B", "B - Beam" },
+            }));
+            currentSchema.AddColumn(new BoolColumn(3));
+            currentSchema.AddColumn(new FileRefColumn(4, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(5, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(6, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(7, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(8, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(9, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(11, "{0}.wav"));
+            currentSchema.AddColumn(new FileRefColumn(13, "{0}.wav"));
+            currentSchema.AddColumn(new FileRefColumn(15, "{0}.wav"));
+            currentSchema.AddColumn(new TwoDARefColumn(25, "visualeffects.2da"));
+            currentSchema.AddColumn(new FileRefColumn(26, "{0}.mdl"));
+            currentSchema.AddColumn(new BoolColumn(27));
+            AddSchema("visualeffects", currentSchema);
+
+            currentSchema = new TwoDASchema();
+            currentSchema.LabelColumn = 1;
+            currentSchema.AddColumn(new StrRefColumn(2));
+            currentSchema.AddColumn(new FileRefColumn(3, "{0}.mdl"));
+            currentSchema.AddColumn(new TwoDARefColumn(4, "lightcolor.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(8, "placeableobjsnds.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(10, "bodybag.2da"));
+            currentSchema.AddColumn(new FileRefColumn(11, "{0}.mdl"));
+            currentSchema.AddColumn(new BoolColumn(13));
+            AddSchema("placeables", currentSchema);
+
+            currentSchema = new TwoDASchema();
+            currentSchema.LabelColumn = 1;
+            currentSchema.AddColumn(new StrRefColumn(2));
+            currentSchema.AddColumn(new FileRefColumn(3, "{0}.tga"));
+            currentSchema.AddColumn(new EnumColumn(5, new string[] { "P", "T", "S", "M", "L", null }, new Dictionary<string, string>()
+            {
+                { "P", "P - Personal" },
+                { "T", "T - Touch" },
+                { "S", "S - Short" },
+                { "M", "M - Medium" },
+                { "L", "L - Long" },
+            }));
+            currentSchema.AddColumn(new EnumColumn(6, new string[] { "-", "v", "s", "vs", null }, new Dictionary<string, string>()
+            {
+                { "-", "- - None" },
+                { "v", "v - Verbal" },
+                { "s", "s - Somatic" },
+                { "vs", "vs - Verbal/Somatic" },
+            }));
+            var flagMetaMagic = new FlagInfo("X2");
+            flagMetaMagic.AddFlag("Empower Spell", 0x01);
+            flagMetaMagic.AddFlag("Extend Spell", 0x02);
+            flagMetaMagic.AddFlag("Maximize Spell", 0x04);
+            flagMetaMagic.AddFlag("Quicken Spell", 0x08);
+            flagMetaMagic.AddFlag("Silent Spell", 0x10);
+            flagMetaMagic.AddFlag("Still Spell", 0x20);
+            currentSchema.AddColumn(new FlagsColumn(7, flagMetaMagic));
+            var flagTarget = new FlagInfo("X2");
+            flagTarget.AddFlag("Self", 0x01);
+            flagTarget.AddFlag("Creature", 0x02);
+            flagTarget.AddFlag("Ground", 0x04);
+            flagTarget.AddFlag("Item", 0x08);
+            flagTarget.AddFlag("Door", 0x10);
+            flagTarget.AddFlag("Placeable", 0x20);
+            flagTarget.AddFlag("Trigger", 0x40);
+            currentSchema.AddColumn(new FlagsColumn(8, flagTarget));
+            currentSchema.AddColumn(new FileRefColumn(19, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(20, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(21, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(22, "{0}.wav"));
+            currentSchema.AddColumn(new FileRefColumn(23, "{0}.wav"));
+            currentSchema.AddColumn(new FileRefColumn(24, "{0}.wav"));
+            currentSchema.AddColumn(new EnumColumn(25, new string[] { "area", "attack", "out", "self", "touch", "up", null }));
+            currentSchema.AddColumn(new FileRefColumn(27, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(28, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(29, "{0}.mdl"));
+            currentSchema.AddColumn(new FileRefColumn(30, "{0}.wav"));
+            currentSchema.AddColumn(new BoolColumn(31));
+            currentSchema.AddColumn(new FileRefColumn(32, "{0}.mdl"));
+            currentSchema.AddColumn(new EnumColumn(33, new string[] { "accelerating", "ballistic", "bounce", "burst", "highballistic", "homing", "linked", "spiral", "test", null }));
+            currentSchema.AddColumn(new EnumColumn(34, new string[] { "hand", "head", "monster0", "monster1", "monster2", "monster3", "monster4", null }));
+            currentSchema.AddColumn(new FileRefColumn(35, "{0}.wav"));
+            currentSchema.AddColumn(new EnumColumn(36, new string[] { "path", "target", null }));
+            currentSchema.AddColumn(new EnumColumn(37, new string[] { "Acid", "Cold", "Death", "Divine", "Disease", "Electricity", "Fear", "Fire", "Mind_Affecting", "Negative", "Poison", "Positive", "Sonic", null }));
+            currentSchema.AddColumn(new BoolColumn(38));
+            currentSchema.AddColumn(new TwoDARefColumn(39, "spells.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(40, "spells.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(41, "spells.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(42, "spells.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(43, "spells.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(44, "categories.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(45, "spells.2da"));
+            currentSchema.AddColumn(new EnumColumn(46, new string[] { "1", "2", "3", "4", null }, new Dictionary<string, string>()
+            {
+                { "1", "1 - Spell" },
+                { "2", "2 - Creature Ability" },
+                { "3", "3 - Feat" },
+                { "4", "4 - Item Power" },
+            }));
+            currentSchema.AddColumn(new StrRefColumn(47));
+            currentSchema.AddColumn(new BoolColumn(48));
+            currentSchema.AddColumn(new BoolColumn(49));
+            currentSchema.AddColumn(new StrRefColumn(50));
+            currentSchema.AddColumn(new BoolColumn(51));
+            currentSchema.AddColumn(new TwoDARefColumn(52, "feat.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(53, "spells.2da"));
+            currentSchema.AddColumn(new TwoDARefColumn(54, "spells.2da"));
+            currentSchema.AddColumn(new BoolColumn(55));
+
+            AddSchema("spells", currentSchema);
         }
     }
 }
